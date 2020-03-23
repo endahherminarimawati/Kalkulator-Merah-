d5964fdd93b1e9d0597238da12455890a1e6cb81
andrinur13 <andrinur13@gmail.com>
2020-03-23 22:58:38 +0700

    aku tambahkan class di main dan penggunaan mehod

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Kalkulator
+{
+public:
+    // tulis fungsinya/methodnya disini ya...
+};
+
+main()
+{
+    Kalkulator kalk;
+
+    // nanti tinggal running aja methodnya
+    // contoh kalk.pertambahan();
+
+}
